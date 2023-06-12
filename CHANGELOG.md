324f58fd437487509f7228ad44d83348f89bc7c8
AndreyBlum <andrey.blum1@gmail.com>
2023-06-12 08:10:56 -0300

    Fixing warps in the row count 5 and 10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG.md
 
+## 1.1.0
+
+# BUGFIX
+- Fixing warps in the row count 5 and 10
+
 ## 1.0.0
 
 # FEATURE
--- src/service/wheat/wheat-farming.py
+++ src/service/wheat/wheat-farming.py
@@ -1,23 +1,30 @@
 import pyautogui
 import time
 
-def harvest_wheat(direction):
+def harvest_wheat(direction, row):
     pyautogui.keyDown("x")
+    if row == 5:
+        pyautogui.keyUp("D")
+        warp_garden()
+        move_180()
+        direction = "left"
+    if row == 10:
+        direction = "right"
+        row = 0
+        warp_garden()
+        return {
+            "direction": "right",
+            "row": row,
+        }
     print(direction)
-    if direction == "right":
-        move_to_right()
-    else:
-        move_to_left()
+    move_to_direction(direction)
+    row = row + 1
+    print(f'You are in a {row}th row')
     time.sleep(90)
-    direction = "left"
-    if direction == "left":
-        move_to_left()
-    else:
-        move_to_right()
-    # Time to harvest all row
-    time.sleep(90)
-    direction = "right"
-    return direction
+    return {
+        "direction": invert_move(direction),
+        "row": row,
+    }
 
 def move_to_left():
     pyautogui.keyUp("D")
@@ -27,8 +34,15 @@ def move_to_right():
     pyautogui.keyUp("A")
     pyautogui.keyDown("D")
 
+def move_to_direction(direction):
+    if direction == "right":
+        move_to_right()
+    else:
+        move_to_left()
+
 def warp_garden():
     pyautogui.press("z")
+    time.sleep(5)
 
 def move_180():
     print("Moving 180 dregrees")
@@ -37,37 +51,43 @@ def move_180():
     pyautogui.keyDown("W")
     time.sleep(0.5)
     pyautogui.keyUp("W")
-    
+
+def invert_move(direction):
+    if direction == "right":
+        print("Inverted to left")
+        return "left"
+    else:
+        print("Inverted to right")
+        return "right"
 def change_window():
     pyautogui.hotkey('alt', 'tab')
     pyautogui.press('esc')
 
 def locate_breaking_events():
     bad_internet = pyautogui.locateCenterOnScreen("src/assets/wheat/bad_internet.png", confidence=0.5)
-    if bad_internet:
+    bad_internet_2 = pyautogui.locateCenterOnScreen("src/assets/wheat/bad_internet_2.png", confidence=0.5)
+    if bad_internet or bad_internet_2:
         print("Bad Internet - Exiting...")
         exit()
 
 def act():
     runs = 0
     row_count = 0
+    direction = "right"
     while(True):
         if runs == 0:
             change_window()
+            runs += 1
         locate_breaking_events()
-        direction = "right"
-        if row_count == 4:
-            direction = "right"
-            row_count = 0
-            warp_garden()
-        if row_count == 2:
-            direction = "left"
-            warp_garden()
-            move_180()
-        direction = harvest_wheat(direction)
-        row_count += 1
-        runs += 1
-        print(f'You are in a {row_count}th row')
+        print(row_count)
+        harvesting = harvest_wheat(direction, row_count)
+        print(harvesting)
+        try:
+            direction = harvesting["direction"]
+            row_count = harvesting["row"]
+        except Exception as err:
+            print(f"Value not found: {err}")
+        print(f"This is the direction {direction} and the row_count {row_count}")
         print(f'You are in the run number {runs}')
 
-act()   
+act()
