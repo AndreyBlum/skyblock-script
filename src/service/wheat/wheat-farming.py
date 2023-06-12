ce080c014da9e42733a5e275ebd57e0cbfe43697
Jaum <antoniojoao67@outlook.com>
2023-06-12 18:37:29 -0300

    atribuindo o valor do argumento duration para uma constante

--- src/service/wheat/wheat-farming.py
+++ src/service/wheat/wheat-farming.py
@@ -45,10 +45,10 @@ def warp_garden():
     time.sleep(5)
 
 def move_180():
-    move_duration = 0.5
+    MOVE_DURATION = 0.5
     print("Moving 180 dregrees")
-    pyautogui.move(800, 0, duration=move_duration)
-    pyautogui.move(400, 0, duration=move_duration)
+    pyautogui.move(800, 0, duration=MOVE_DURATION)
+    pyautogui.move(400, 0, duration=MOVE_DURATION)
     pyautogui.keyDown("W")
     time.sleep(0.5)
     pyautogui.keyUp("W")
